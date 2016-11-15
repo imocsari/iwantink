0f9c4999982d97a40db856aef1e8fae16035f028
nicholassiskos <nicholasasiskos@gmail.com>
2016-11-15 16:52:26 +0000

    Artists page update

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ User.create([{email: "ajkdad@ds.com", password: "fdsfsdfdssfdsf", firstname: "al
 
 Artist.create([{ first_name: 'Antonio', last_name: 'Smith', email: 'smith_antonio59@hotmail.com', password: "antonio", bio: 'I am 27 year old experienced tattoo artist with 10 years experience, I am experienced in all styles', }])
 Artist.create([{ first_name: 'Gab', last_name: 'Del', email: 'gabdelaun@mail.com', password: "antonio", bio: 'I am 27 year old experienced tattoo artist with 10 years experience, I am experienced in all styles', }])
+Artist.create([{ first_name: 'Nicholas', last_name: 'Andreas', email: 'nicholas@mail.com', password: "nicholas", bio: 'I am 27 year old experienced tattoo artist with 10 years experience, I am experienced in all styles', }])
+
+Artist.create([{ first_name: 'Imre', last_name: 'Paul', email: 'imre59@hotmail.com', password: "antonio", bio: 'I am 27 year old experienced tattoo artist with 10 years experience, I am experienced in all styles', }])
+Artist.create([{ first_name: 'Paul', last_name: 'Daniels', email: 'pauldaniels@mail.com', password: "antonio", bio: 'I am 27 year old experienced tattoo artist with 10 years experience, I am experienced in all styles', }])
+Artist.create([{ first_name: 'Pedro', last_name: 'Sammone', email: 'pedro@mail.com', password: "nicholas", bio: 'I am 27 year old experienced tattoo artist with 10 years experience, I am experienced in all styles', }])
