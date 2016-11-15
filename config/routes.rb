bdc834366b391181cc1a9066e8617b892ef592ab
gabdelaun <gabdelaun@gmail.com>
2016-11-15 12:07:29 +0000

    controller artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/new'
+
+  get 'artists/update'
+
+  get 'artists/create'
+
+  get 'artists/destroy'
+
   devise_for :artists
   devise_for :users
   root to: 'pages#home'
