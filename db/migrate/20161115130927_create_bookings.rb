f1bda4c68afc2125bb23319a32218553eab23f72
Imre Mocsari <imremocsari1@yahoo.co.uk>
2016-11-15 14:33:22 +0000

    links on buttons

---
+++ db/migrate/20161115130927_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[5.0]
+  # def change
+  #   create_table :bookings do |t|
+  #     t.references :user_id, foreign_key: true
+  #     t.references :artist_id, foreign_key: true
+  #     t.datetime :starts_at
+  #     t.datetime :confirmed_at
+  #
+  #     t.timestamps
+  #   end
+  # end
+end
