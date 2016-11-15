cebcfd6e2cd9b5a307400d2a1527189032539dab
Imre Mocsari <imremocsari1@yahoo.co.uk>
2016-11-15 12:10:24 +0000

    routes and user controller changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :artists
   devise_for :users
   root to: 'pages#home'
+
+  # resources  :users # only: [:show, :new, :create, :index, ]
+  resources :artists #only: [:show, :new, :create, :index,:destroy]
+  resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
