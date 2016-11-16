541a0d738d7449944c837b2ab0529fc1325c2a98
gabdelaun <gabdelaun@gmail.com>
2016-11-16 14:14:59 +0000

    changment

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,8 +2,15 @@ class Artist < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  validates :first_name, :last_name, presence: true, uniqueness: true
-  has_attachment :picture
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, presence: true, uniqueness: true
+
+  has_attachment :picture
+  has_attachments :photos
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
