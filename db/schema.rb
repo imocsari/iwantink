e19be04779f71ea2e054e28496a22dce49020cf3
gabdelaun <gabdelaun@gmail.com>
2016-11-17 15:08:04 +0000

    fix google url

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161117132526) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +69,7 @@ ActiveRecord::Schema.define(version: 20161117132526) do
     t.string   "email"
     t.string   "contact"
     t.string   "tattoo_place"
-    t.string   "budget"
+    t.integer  "budget"
     t.datetime "prefered_date"
     t.text     "description"
     t.index ["artist_id"], name: "index_bookings_on_artist_id", using: :btree
